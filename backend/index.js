46a40ec07427cc3fbe2f51ee8aacb83e3ae5992d
Paarth Kothari <paarthkothari14@gmail.com>
2022-02-21 23:20:27 +0530

    Created Routes & Schema for Storing Notes & Users

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,15 @@ const express = require("express");
 connectToMongo();
 const app = express();
 const port = 3000;
-app.get("/", (req, res) => {
-  res.send("Hello Paarth");
-});
+
+// app.get("/", (req, res) => {
+//   res.send("Hello Paarth");
+// });
+
+// Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
