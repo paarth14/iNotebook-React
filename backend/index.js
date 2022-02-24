24204d1d2e40f1c3d01ec04cf7460f2bf24cd24a
Paarth Kothari <paarthkothari14@gmail.com>
2022-02-24 12:42:36 +0530

    Storing Data into the Database Using Mongoose Model

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,7 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello Paarth");
-// });
+app.use(express.json());
 
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
